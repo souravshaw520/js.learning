01fe7e7a7854e9d86274dc5bf5658533b548c853
souravshaw520 <souravshaw520@gmail.com>
2024-12-26 04:23:46 +0000

    config

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -3,5 +3,6 @@ let accountEmail = "sourav.shaw@gmail.com"
 var accountPassword = "121212"
 accountCity = "Jaipur"
 
-console.table([accountId, accountEmail, accountPassword, accountCity]);
+console.log(accountId);
 
+console.table([accountId, accountEmail, accountPassword, accountCity])
